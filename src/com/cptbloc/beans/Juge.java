bca8a184ee67840101372c4fa6bafb88bf0e473c
dridrii <marmy.adrien@gmail.com>
2015-08-18 07:25:46 +0200

    Update Juge.java
    implements Serializable

--- src/com/cptbloc/beans/Juge.java
+++ src/com/cptbloc/beans/Juge.java
@@ -1,6 +1,8 @@
 package com.cptbloc.beans;
 
-public class Juge {
+import java.io.Serializable;
+
+public class Juge implements Serializable{
     private Long   id;
     private String pseudo;
     private String nom;
